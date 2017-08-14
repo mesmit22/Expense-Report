e1305b89abb726b1c16fc62bafaa9f289745755c
Mark Smith <mark.edwin.smith@gmail.com>
2017-08-14 17:55:09 -0400

    Address model added

---
+++ ExpenseReport/ExpenseReport/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseReport.Models
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
