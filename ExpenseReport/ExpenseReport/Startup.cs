81c98351cd735203ae45c6aff90a6093568005e7
Mark Smith <mark.edwin.smith@gmail.com>
2017-08-14 17:19:18 -0400

    Git Init

---
+++ ExpenseReport/ExpenseReport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseReport.Startup))]
+namespace ExpenseReport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
