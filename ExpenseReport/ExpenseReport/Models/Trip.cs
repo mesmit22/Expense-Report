a709b4fbe87c6908fbbb4cd82633497357b0ba0b
Mark Smith <mark.edwin.smith@gmail.com>
2017-08-15 15:09:30 -0400

    Trips controller and view added

---
+++ ExpenseReport/ExpenseReport/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseReport.Models
+{
+    public class Trip
+    {
+        public int TripID { get; set; }
+        [Display(Name = "Starting Address")]
+        public Address StartingAddress { get; set; }
+        [Display(Name = "Ending Addres")]
+        public Address EndingAddress { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
